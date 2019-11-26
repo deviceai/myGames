f75dd0dec18054b0f9b77cb493f941ba6663a307
deviceai <device.ai@gmail.com>
2019-11-26 21:12:19 +0200

    empty window

--- JavaSweeper/src/JavaSweeper.java
+++ JavaSweeper/src/JavaSweeper.java
@@ -1,7 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+
+    private JPanel panel;
+
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
+    }
+
+    private  JavaSweeper(){
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel(){
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,500));
+        add(panel);
+    }
+
+    private void initFrame(){
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("JavaSweeper");
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
     }
 }
